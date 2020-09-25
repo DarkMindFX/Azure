2f2870e1eaaf8888f54d7e2851025d901c91b816
DarkMindFx <contact@darkmindfx.com>
2020-09-25 11:29:56 +0200

    Adding UT for MsgBus.Azure

---
+++ SampleBatch/Tests/Test.MsgBus.Azure.NETCore/MsgBusContextParams.cs
@@ -0,0 +1,34 @@
+using MsgBus.Azure;
+using NUnit.Framework;
+using SampleBatch.Interfaces;
+
+namespace Test.MsgBus.Azure.NETCore
+{
+    public class MsgBusContextParams
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PrepareParams_Success()
+        {
+            IMsgBusContextParams ctxParams = GetContextParams();
+
+            Assert.IsNotNull(ctxParams);
+            Assert.IsNotNull(ctxParams.Parameters);
+            Assert.IsNotEmpty(ctxParams.Parameters);
+            Assert.IsTrue(ctxParams.Parameters.Count == 3);
+            Assert.IsTrue(ctxParams.Parameters.ContainsKey("StorageAccountName"));
+            Assert.IsTrue(ctxParams.Parameters.ContainsKey("StorageAccountKey"));
+            Assert.IsTrue(ctxParams.Parameters.ContainsKey("MessageQueue"));
+        }
+
+        private IMsgBusContextParams GetContextParams()
+        {
+            MsgBusContext ctx = new MsgBusContext();
+            return ctx.PrepareParams();
+        }
+    }
+}
